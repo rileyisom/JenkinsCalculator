96958bfff04a55ad805ef86e16c1123b154a0c4c
Riley Isom <rileyisom99@gmail.com>
2022-02-15 22:22:20 -0700

    Add all method implementations

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,4 @@
+import java.util.UUID;
 
 class Calculator {
 
@@ -63,8 +64,8 @@ class Calculator {
 
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
-    String createUniqueID(String n){
-        return null;
+    String createUniqueID(String n) {
+        return n + UUID.randomUUID().toString();
     }
 
 
